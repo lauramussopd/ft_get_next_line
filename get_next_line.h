f25f2c0a6d29796a2504efa54ff1087a20c4b1ca
Laura Musso <laurmuss@car8s5.42barcelona.com>
2023-08-09 19:56:45 +0200

    wed756

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,24 @@
 /*   By: laurmuss <laurmuss@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 16:36:14 by laurmuss          #+#    #+#             */
-/*   Updated: 2023/08/09 16:38:08 by laurmuss         ###   ########.fr       */
+/*   Updated: 2023/08/09 19:43:16 by laurmuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-#define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <stdlib.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
+char    *get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *str, int c);
+void	*true_free(char **buffer);
+char	*ft_strjoin(char *buffer, char *tail, ssize_t size);
 
 #endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laurmuss <laurmuss@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/09 19:35:35 by laurmuss          #+#    #+#             */
+/*   Updated: 2023/08/09 19:39:51 by laurmuss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	l;
+
+	l = 0;
+	while (s[l] != '\0')
+		l++;
+	return (l);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if (str && *str)
+    {
+	while (*str && *str != c)
+		str++;
+	if (!*str && c != '\0')
+		return (0);
+    }
+    return ((char *) str);
+
+}
+
+void	*true_free(char **buffer)
+{
+	if (buffer && *buffer) //lo que t mando y el contenido de lo que te mando
+	{
+		free(*buffer);
+		*buffer = NULL;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *buffer, char *tail, ssize_t size)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	if (!buffer)
+		buffer = malloc(sizeof(char));
+	if (!buffer)
+		return (NULL);
+	tmp = malloc((ft_strlen(buffer) + size + 1) * sizeof(char)); //lo que tiene el buffer + algo
+	if (!tmp)
+		return (true_free(&buffer));
+	i = -1;
+	while (buffer[++i])
+		tmp[i] = buffer[i];
+	j = -1;
+	while (tail[++j])
+		tmp[i++] = tail[j];
+	true_free(&buffer);
+    tmp[i] = '\0';
+	return (tmp);
+}
+
